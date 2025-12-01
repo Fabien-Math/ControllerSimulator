Fix SMC (sign error), improve variable names and add data document about thrusters

src/controller_manager.py
@@ -7,9 +7,9 @@ from thruster_system import ThrusterSystem
 
 class ControllerManager:
 	def __init__(self, robot, controller_params: dict, thruster_system: ThrusterSystem):
-		self.desired_tfs = []
-		self.desired_tf = None
-		self.last_desired_tf = None
+		self.desired_etas = []
+		self.desired_eta = None
+		self.last_desired_eta = None
 		self.mission_finished = False
 
 		self.etas_err_world = np.zeros(6)
@@ -33,61 +33,45 @@ class ControllerManager:
 	def add_waypoint(self, wps, overwrite = False):
 		if overwrite:
 			if len(np.shape(wps) > 1):
-				self.desired_tfs = wps
+				self.desired_etas = wps
 			else:
-				self.desired_tfs = [wps]
+				self.desired_etas = [wps]
 		else:
 			if len(np.shape(wps)) > 1:
 				for wp in wps:
-					self.desired_tfs.append(wp)
+					self.desired_etas.append(wp)
 			else:
-				self.desired_tfs.append(wps)
+				self.desired_etas.append(wps)
 	
 
 	def manage_waypoint(self, eta, nu):
-		if self.desired_tf is not None:
+		if self.desired_eta is not None:
 			if np.all(np.abs(self.etas_err) < self.eta_tol) and np.all(np.abs(self.nus_err) < self.nu_tol) :
-				self.last_desired_tf = self.desired_tf
+				self.last_desired_eta = self.desired_eta
 				# print('Arrived')
-				self.desired_tf = None
+				self.desired_eta = None
 			else:
 				return
 		
-		if self.desired_tf is None:
-			if len(self.desired_tfs):
-				self.desired_tf = self.desired_tfs.pop(0)
+		if self.desired_eta is None:
+			if len(self.desired_etas):
+				self.desired_eta = self.desired_etas.pop(0)
 			else:
 				self.mission_finished = True
-				if self.last_desired_tf is not None:
-					self.desired_tf = self.last_desired_tf
+				if self.last_desired_eta is not None:
+					self.desired_eta = self.last_desired_eta
 				else:
-					self.desired_tf = eta
+					self.desired_eta = eta
 	
 
 	def compute_error(self, eta, nu):
-		self.etas_err_world = self.desired_tf - eta
-		eta_err = self.desired_tf - eta
-		
-		J1 = R.from_euler('xyz', eta[3:]).as_matrix()
-		R_d = R.from_euler('xyz', self.desired_tf[3:]).as_matrix()
-		S = 0.5 * (R_d.T @ J1 - J1.T @ R_d).T   # skew-symmetric matrix
-
-		eta_err[:3] = J1.T @ eta_err[:3]
-
-		# eta_err[3:] = np.array([
-		# 	S[2, 1],
-		# 	S[0, 2],
-		# 	S[1, 0]
-    	# ])
-
-
-		# if np.linalg.norm(eta_err[:3]) > 1.0:
-		# 	eta_err[:3] /= np.linalg.norm(eta_err[:3])
-		# if np.linalg.norm(eta_err[3:]) > 0.5:
-		# 	eta_err[3:] /= np.linalg.norm(eta_err[3:])
+		self.etas_err_world = self.desired_eta - eta
+		eta_err = np.zeros(6)
+		eta_err[:3] = self.robot.J1.T @ (self.desired_eta[:3] - eta[:3])
+		eta_err[3:] = self.desired_eta[3:] - eta[3:]
 
 		self.etas_err = eta_err
-		self.nus_err = nu
+		self.nus_err = - nu
 	
 
 	def update(self, dt, eta, nu):

src/graphical_simulation_manager.py
@@ -3,6 +3,7 @@ from robot import Robot
 from viewer.viewer import Viewer
 import tqdm
 import numpy as np
+import sys
 
 class GraphicalSimulationManager:
 	def __init__(self, simulation_params, robot_params, environment_params):
@@ -15,18 +16,28 @@ class GraphicalSimulationManager:
 
 		self.viewer = Viewer(self.robot, self.dt)
 
+
 	def simulate(self):
+		
+		failed = False
+
 		for i in tqdm.tqdm(range(int(self.end_time/self.dt))):
 			self.robot.update(self.dt, self.ocean)
 
-
-			if np.isnan(np.sum(self.robot.eta)) or np.linalg.norm(self.robot.nu1) > 4  or np.linalg.norm(self.robot.nu2) > np.pi:
-				print(f"Mission failed!\nRobot fucked up!")
+			if np.isnan(np.sum(self.robot.eta)) or np.linalg.norm(self.robot.nu1) > 4  or np.linalg.norm(self.robot.nu2) > 2 * np.pi:
+				failed = True
 				break
 
 			if self.robot.controller.mission_finished:
-				print(f"Mission accomplished!\nTotal simulation time: {i*self.dt:.3f} s")
 				break
+
+		if failed:
+			sys.stderr.write("Mission FAILED!\nRobot fucked up!\n")
+
+		if self.robot.controller.mission_finished:
+			print(f"Mission accomplished!\nTotal simulation time: {i*self.dt:.3f} s")
+		else:
+			print(f"Mission NOT finished!\nTotal simulation time: {i*self.dt:.3f} s")
 
 		self.robot.logger.np_format()
 

src/logging_system.py
@@ -17,7 +17,7 @@ class LoggingSystem:
 		self.etas_err = []
 		self.etas_err_world = []
 		self.nus_err = []
-		self.desired_tfs = np.array(robot.controller.desired_tfs)
+		self.desired_etas = np.array(robot.controller.desired_etas)
 		self.timestamps = []
 
 		self.errs = False
@@ -29,7 +29,7 @@ class LoggingSystem:
 		self.nus.append(np.array(self.robot.nu))
 		self.forces.append(np.array(self.robot.forces))
 		self.hydro_forces.append(np.array(self.robot.hydro_forces))
-		self.etas_desired.append(np.array(self.robot.controller.desired_tf))
+		self.etas_desired.append(np.array(self.robot.controller.desired_eta))
 		self.nus_desired.append(np.zeros(6))
 		
 		if self.thrust_force is not None:

src/pid_controller.py
@@ -14,7 +14,7 @@ class PIDController:
 
 	def update(self, dt, eta_err, nu_err):
 		self.int_eta_err += eta_err * dt
-		self.int_eta_err = np.clip(self.int_eta_err, -5, 5)
+		self.int_eta_err = np.clip(self.int_eta_err, -10, 10)
   
 		if dt:
 			self.der_eta_err = (eta_err - self.last_eta_err ) / dt

src/robot.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 
-from controller import ControllerManager
+from controller_manager import ControllerManager
 from thruster_system import ThrusterSystem
 from logging_system import LoggingSystem
 
@@ -68,7 +68,7 @@ class Robot:
 
 		self.thrusters = ThrusterSystem(robot_params["thruster"])
 		self.controller = ControllerManager(self, robot_params["controller"], self.thrusters)
-		self.controller.desired_tfs = list(robot_params['mission'])
+		self.controller.desired_etas = list(robot_params['mission'])
 
 		self.time = 0
 		self.log = True
@@ -127,6 +127,8 @@ class Robot:
 		self.forces = self.coriolis_centripetal_forces + self.hydro_forces + T_eta + G_eta
 		# Update acceleration
 		self.gamma = np.matmul(M_inv_eta, self.forces)
+		# np.set_printoptions(threshold=np.inf, linewidth=np.inf)
+		# print(self.forces)
 
 
 	def compute_J1(self):

src/simulation_parser.py
@@ -28,13 +28,13 @@ def load_config(filename):
 	thrusters_cfg = robot_cfg.get('thrusters', {})
 	n_thursters = int(thrusters_cfg.get('n_thrusters'))
 	
-	thrusters = [{"name": "", "position": None, "force_limits": None, "wn": 0, "zeta": 0} 
+	thrusters = [{"name": "", "position": None, "thrust_limits": None, "wn": 0, "zeta": 0} 
 			  		for _ in range(n_thursters)]
 	for i in range(n_thursters):
 		thruster_cfg = thrusters_cfg.get('thruster'+ str(i), {})
 		thrusters[i]["name"] = thruster_cfg.get('name', "thruster" + str(i))
 		thrusters[i]["position"] = np.array(thruster_cfg.get('position', None))
-		thrusters[i]["force_limits"] = np.array(thruster_cfg.get('limits', None))
+		thrusters[i]["thrust_limits"] = np.array(thruster_cfg.get('thrust_limits'))
 		thrusters[i]["wn"] = np.array(thruster_cfg.get('wn', 0))
 		thrusters[i]["zeta"] = np.array(thruster_cfg.get('zeta', 0))
 

src/sliding_mode_controller.py
@@ -7,13 +7,18 @@ class SlidingModeController:
 		self.lambda_smc = controller_params['lambda_smc']
 		self.phi = controller_params['phi']
 
+		self.robot = robot
+
 		self.s = np.zeros(6)
 		self.u = np.zeros(6)
 		self.cmd = np.zeros(6)
 
 	def update(self, dt, eta_err, nu_err):
-		self.s = self.lambda_smc * (-eta_err) + nu_err
-		self.u = - self.k_smc * np.array([np.tanh(si / self.phi[i]) for i, si in enumerate(self.s)])
+		self.s = self.lambda_smc * (eta_err) + nu_err
+		# np.set_printoptions(threshold=np.inf, linewidth=np.inf)
+		# print(self.s)
+		self.u = self.k_smc * np.array([np.tanh(si / self.phi[i]) for i, si in enumerate(self.s)])
+		# print(self.u)
 		self.cmd = self.u
 
 	

src/thruster_system.py
@@ -8,8 +8,7 @@ class ThrusterSystem:
 
 		self.names = np.array([t['name'] for t in self.thrusters])
 		self.positions = np.array([t['position'] for t in self.thrusters])
-		self.limits = np.array([t['force_limits'] for t in self.thrusters])
-		
+		self.thrust_limits = np.array([t['thrust_limits'] for t in self.thrusters])
 		# 2nd-order actuator model for each thruster: x'' + 2ζωx' + ω²x = ω²u
 		self.wn = np.array([t['wn'] for t in self.thrusters])
 		self.zeta = np.array([t['zeta'] for t in self.thrusters])
@@ -53,21 +52,18 @@ class ThrusterSystem:
 			
 			self.T[:, i] = [fx, fy, fz, tx, ty, tz]
 
-
 	def update(self, dt, u_cmd):
 		# Compute desired thruster forces using pseudo-inverse control allocation
 		thruster_cmd = self.T_inv @ u_cmd
-		
-
 		# Clip desired thruster commands to actuator physical limits
-		thruster_cmd = np.clip(thruster_cmd, self.limits[:, 0], self.limits[:, 1])
+		thruster_cmd = np.clip(thruster_cmd, self.thrust_limits[:, 0], self.thrust_limits[:, 1])
 
 		# Update thruster forces using second-order propeller dynamics
-		# Discretize using explicit Euler (or better with RK4 if you want more accuracy)
+		# Discretize using explicit Euler
 		accel = self.wn**2 * (thruster_cmd - self.thrust) - 2 * self.zeta * self.wn * self.rpm
 		self.rpm += accel * dt
 		self.thrust += self.rpm * dt
 
 		# Clip again to be sure the physical force doesn't exceed limits
-		self.force = self.T @ np.clip(self.thrust, self.limits[:, 0], self.limits[:, 1]) 
+		self.force = self.T @ np.clip(self.thrust, self.thrust_limits[:, 0], self.thrust_limits[:, 1]) 
 

src/viewer/viewer.py
@@ -23,7 +23,7 @@ class Viewer:
 		# Playback & animation
 		self.etas = None
 		self.nus = None
-		self.desired_tfs = None
+		self.desired_etas = None
 		self.times = None
 		self.frame_index = 0
 		self.frame_index_float = 0
@@ -58,7 +58,7 @@ class Viewer:
 	def load_sequences(self):
 		self.etas = self.robot.logger.etas
 		self.nus = self.robot.logger.nus
-		self.desired_tfs = self.robot.logger.desired_tfs
+		self.desired_etas = self.robot.logger.desired_etas
 		self.times = self.robot.logger.timestamps
 
 	def draw_target_marker(self, size=0.05):
@@ -110,7 +110,7 @@ class Viewer:
 		
 		for i in range(thrusters.n_thrusters):
 			thruster_pos = robot_pos + rotation_matrix @ thrusters.positions[i, :3]
-			max_abs_thrust = np.max(np.abs(thrusters.limits[i]))
+			max_abs_thrust = np.max(np.abs(thrusters.thrust_limits[i]))
 			thrust = self.robot.logger.thruster_forces[self.frame_index, i]
 			thrust_ratio = thrust / max_abs_thrust
 			max_thrust_pos = robot_pos + rotation_matrix @ (thrusters.positions[i, :3] + length * thrust_ratio * thrusters.T[:3, i])
@@ -220,8 +220,8 @@ class Viewer:
 			self.draw_thrusters_thrust(draw_on_top=True);
 
 		if self.gui.draw_wps_button.active:
-			if len(self.desired_tfs):
-				for tf in self.desired_tfs:
+			if len(self.desired_etas):
+				for tf in self.desired_etas:
 					glPushMatrix()
 					glTranslatef(*tf[:3])
 					glRotatef(tf[5] * RAD2DEG, 0, 0, 1)